bd1401701f7bbca3466068e3b535f03c2be457b4
Barney Carroll <barneycarroll@users.noreply.github.com>
2016-02-17 15:23:12 +0000

    Half-arsed attempt at #2

--- src/plasm.es3.js
+++ src/plasm.es3.js
@@ -1,41 +1,62 @@
-'use strict'
+var plasm = ( function(){
+  'use strict'
+  
+  if( require && require instanceof Function ){
+    require( 'es6-collections' )
 
-require( 'es6-collections' )
+    var DeepMap = require( 'deepmap' )
+  }
+  
+  // A component is added to the trail when it's invoked,
+  // thus any component can see where it is nested.
+  var breadcrumbs = []
+  // Structure for identifying instances based on multiple complex keys
+  var instances   = new DeepMap()
+  
+  // Pass in:
+  // * The component factory
+  // * The input you want to pass to it
+  // * A key (none for unique, index for lists)
+  //   identifies the instance within the calling context
+  return function invoke( component, input, key ){
+    // Construct a composite retrieval key to uniquely identify our instance
+    var keys = new Array( breadcrumbs.length + 2 )
 
-var give    = require( 'xet' )
-var DeepMap = require( 'deepmap' )
+    // Based on 
+    // 1. Breadrcumbs (instances it sits within)
+    for( var i = 0; i < breadcrumbs.length; i++ )
+      keys[ i ] = breadcrumbs[ i ]
 
-// A component is added to the trail when it's invoked,
-// thus any component can see where it is nested.
-var breadcrumbs = new Set()
-// Structure for identifying instances based on multiple complex keys
-var instances   = new DeepMap()
+    // The component reference
+    keys[ ++i ] = component
+    // Supplied key
+    keys[ ++i ] = key
 
-// Pass in:
-// * The component factory
-// * The input you want to pass to it
-// * A key (none for unique, index for lists)
-//   identifies the instance within the calling context
-module.exports = function invoke( component, input, key ){
-	// Retrieve it if it exists, set it if not
-	var instance = give.call(
-		instances,
-		// These elements allow us to store and retrieve
-		// individual component instances
-		breadcrumbs.concat( component, key ),
-		function initialise(){
-			return new component( input, key )
-		}
-	)
+    var instance
 
-	// Add to the breadcumb trail for nesting context
-	breadcrumbs.add( instance )
+    if( instances.has( keys ) )
+      instance = instances.get( keys )
 
-	// Any nested components will add to it
-	const output = instance.call( instance, input, key )
+    else{
+      instance = function initialise(){
+        return new component( input, key )
+      }
 
-	// Remove from the trail after view execution
-	breadcrumbs.delete( instance )
+      instances.set( keys, instance )
+    }
+  
+    // Add to the breadcumb trail for nesting context
+    breadcrumbs.add( instance )
+  
+    // Any nested components will add to it
+    var output = instance.call( instance, input, key )
+  
+    // Remove from the trail after view execution
+    breadcrumbs.delete( instance )
+  
+    return output
+  }
+}() )
 
-	return output
-}
+if( module && module.exports )
+  module.exports = plasm;
